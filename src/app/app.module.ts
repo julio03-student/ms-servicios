abada6ad01318027382e522baf2e1e9f49d9ac22
juliorc03-student <julianrivera003@gmail.com>
2021-11-23 22:59:55 -0500

    Contianuación de creación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './public/template/header/header.component';
+import { FooterComponent } from './public/template/footer/footer.component';
+import { NavbarComponent } from './public/template/navbar/navbar.component';
+import { NotFoundComponent } from './public/errors/not-found/not-found.component';
+import { InternalServerErrorComponent } from './public/errors/internal-server-error/internal-server-error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    InternalServerErrorComponent
   ],
   imports: [
     BrowserModule,
