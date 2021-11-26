c3fa6bde44281d5f7fcb9b8c886068386ce34e4c
juliorc03-student <julianrivera003@gmail.com>
2021-11-26 18:29:01 -0500

    Listo todo hasta el video 36, Login, Logout

--- src/app/modules/security/general/logout/logout.component.ts
+++ src/app/modules/security/general/logout/logout.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionData } from 'src/app/models/session-data-model';
+import { LocalStorageService } from 'src/app/services/share/local-storage.service';
+import { SecurityService } from 'src/app/services/share/security.service';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private localStorageService: LocalStorageService,
+    private securityService: SecurityService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.localStorageService.RemoveSessionData()
+    this.securityService.RefreshSessionData(new SessionData())
+    this.router.navigate(["/home"])
   }
 
 }
